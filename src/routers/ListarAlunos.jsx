6a64ee4054e7f50b25de3e2c44040748557a5c33
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-30 11:48:56 -0300

    api

---
+++ src/routers/ListarAlunos.jsx
@@ -0,0 +1,11 @@
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom';
+import {FaEdit, FaTrash} from 'react-icons/fa';
+
+function ListarAlunos(){
+    return(
+        <>
+        </>
+    )
+}
+export default ListarAlunos;
